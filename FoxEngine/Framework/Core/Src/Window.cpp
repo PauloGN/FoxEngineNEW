f9418d76332a06ae4d769a5b5b435bd2575c67ed
PauloGN <paulorsantos.gnt@outlook.com>
2023-06-18 14:58:01 -0700

    [FoxEngine Week02] Setting up window creation Setting up windowMessageHandler class

---
+++ FoxEngine/Framework/Core/Src/Window.cpp
@@ -0,0 +1,98 @@
+#include "Precompiled.h"
+#include "Window.h"
+
+using namespace FoxEngine::Core;
+
+LRESULT CALLBACK WindowMessageHandler(HWND handle, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message)
+	{
+		//listening to the destroy, it means if we hit the X button it will close the application.
+		case WM_DESTROY:
+		{
+			PostQuitMessage(0);
+			return 0;
+		}
+	}
+
+	return DefWindowProc(handle, message, wParam, lParam);
+}
+
+//ADJJ
+void FoxEngine::Core::Window::Initialize(HINSTANCE instance, const std::wstring& appName, uint32_t width, uint32_t height)
+{
+	mInstance = instance;
+	mAppName = appName;
+
+	WNDCLASSEX wcex;
+
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WindowMessageHandler;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = instance;
+	wcex.hIcon = LoadIcon(nullptr, IDI_APPLICATION);//top left corner when open up the window
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);//default windows cursor
+	wcex.hbrBackground = static_cast<HBRUSH>(GetStockObject(BLACK_BRUSH));
+	wcex.lpszMenuName = nullptr;
+	wcex.lpszClassName = mAppName.c_str();
+	wcex.hIconSm = LoadIcon(nullptr, IDI_APPLICATION);
+
+	//After setting up the window parameters we have to register and pass by reference
+	RegisterClassExW(&wcex);
+
+	mScreenRec = { 0, 0, static_cast<LONG>(width), static_cast<LONG>(height) };
+	AdjustWindowRect(&mScreenRec, WS_OVERLAPPEDWINDOW, FALSE);
+
+	const int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+	const int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+	const int winWidth = std::min(static_cast<int>(mScreenRec.right - mScreenRec.left), screenWidth);
+	const int winHeight = std::min(static_cast<int>(mScreenRec.bottom - mScreenRec.top), screenHeight);
+
+	const int left = (screenWidth - winWidth) / 2;
+	const int top = (screenHeight - winHeight) / 2;
+
+	mScreenRec.top = top;
+	mScreenRec.left = left;
+
+	mWindow = CreateWindow(
+		mAppName.c_str(),
+		mAppName.c_str(),
+		WS_OVERLAPPEDWINDOW,
+		left,top,
+		winWidth,winHeight,
+		nullptr, nullptr,
+		instance, nullptr
+		);
+
+	ShowWindow(mWindow, SW_SHOWNORMAL);
+	SetCursorPos(screenWidth / 2, screenHeight / 2);
+
+	mActive = (mWindow != nullptr);
+}
+
+void FoxEngine::Core::Window::Terminate()
+{
+	DestroyWindow(mWindow);
+	UnregisterClass(mAppName.c_str(), mInstance);
+	mWindow = nullptr;
+	mInstance = nullptr;
+}
+
+void FoxEngine::Core::Window::ProcessMessage()
+{
+	MSG msg{};
+
+	while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+
+		if (WM_QUIT == msg.message)
+		{
+			mActive = false;
+		}
+	}
+}
