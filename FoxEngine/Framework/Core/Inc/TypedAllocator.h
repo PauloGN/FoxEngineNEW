2b555d84ce7ed49c5b268a4a9e1b80790e0e34da
PauloGN <paulorsantos.gnt@outlook.com>
2024-02-10 11:48:30 -0800

    Week 07 component and services

--- FoxEngine/Framework/Core/Inc/TypedAllocator.h
+++ FoxEngine/Framework/Core/Inc/TypedAllocator.h
@@ -7,18 +7,18 @@ namespace FoxEngine::Core
 	template<class DataType>
 	class TypedAllocator : private BlockAllocator
 	{
+
 	public:
+
 		TypedAllocator(const char* name, size_t capacity):
 			BlockAllocator(name, sizeof(DataType), capacity)
-		{
-
-		}
+		{}
 
 		template<class...Args>
 		DataType* New(Args&&... args)
 		{
 			DataType * instance = static_cast<DataType*>(Allocate());
-			// placement new, calls constructor on desiganted location
+			// placement new, calls constructor on designated location
 			new(instance) DataType(std::forward<Args>(args)...);
 
 			return instance;
